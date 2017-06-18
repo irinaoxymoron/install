afc934493169039a524337e17d1fc03095ab5281
ilya chizhov <ilya1991chizhoiv@gmail.com>
2017-06-18 20:52:45 +0300

    test3

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -42,42 +42,41 @@
 let interNational = {
     russian: {
         name: 'Ирина Андрианова',
-        range: 'UX/UI дизайнер',
-        aboutTitle: 'Немного обо мне',
-        aboutText: 'Принято считать, что дизайнер и разработчик это две разные специальности. На самом деле, хороший специалист по юзабилити всегда немного программист, аналитик, вообще много думающий и изучающий человек. В этом и скрыта главная прелесть работы UX-дизайнера — постоянный рост и стремление к лучшим результатам. В данный момент меня интересуют масштабные проекты, предоставляющие такую возможность.',
-        aboutText2: 'Вы можете связаться со мной по любым вопросам  относительно ',
-        aboutText3: 'моего резюме',
-        aboutText4: 'Перейдем к работам.',
-        workTitle: `Портфолио`,
-        design: 'Дизайн',
-        prototype: 'Прототипирование',
-        coding: 'HTML-верстка',
-        newsletter: 'Email-рассылки',
-        skillsTitle: 'Навыки'
+        //range: 'UX/UI дизайнер',
+        //aboutTitle: 'Немного обо мне',
+        //aboutText: 'Принято считать, что дизайнер и разработчик это две разные специальности. На самом деле, хороший специалист по юзабилити всегда немного программист, аналитик, вообще много думающий и изучающий человек. В этом и скрыта главная прелесть работы UX-дизайнера — постоянный рост и стремление к лучшим результатам. В данный момент меня интересуют масштабные проекты, предоставляющие такую возможность.',
+        //aboutText2: 'Вы можете связаться со мной по любым вопросам  относительно ',
+        //aboutText3: 'моего резюме',
+        //aboutText4: 'Перейдем к работам.',
+        //workTitle: `Портфолио`,
+        //design: 'Дизайн',
+        //prototype: 'Прототипирование',
+        //coding: 'HTML-верстка',
+        //newsletter: 'Email-рассылки',
+        //skillsTitle: 'Навыки'
     },
     english: {
         name: 'Irina Andrianova',
-        range: 'UX/UI designer',
-        aboutTitle: 'Let me introduce myself',
-        aboutText: `It is accepted that a designer and a web developer are two different specialties. To be honest, I have a different point of view. A good UX-specialist should know the basics of programming, should be able to analyze, must constantly grow and learn something new. And I think that it is very inspiring to work! And now I would like to work in a long-term project with interesting new tasks. I hope that my skills can be interesting for your company.`,
-        aboutText2: `Please, contact me for any information relevant to `,
-        aboutText3: `my resume`,
-        aboutText4: `let's move on my works.`,
-        workTitle: `My works`,
-        design: `Design`,
-        prototype: `Prototype`,
-        coding: `Html coding`,
-        newsletter: `Email newsletter`,
-        skillsTitle: `Skills`
+        //range: 'UX/UI designer',
+        //aboutTitle: 'Let me introduce myself',
+        //aboutText: `It is accepted that a designer and a web developer are two different specialties. To be honest, I have a different point of view. A good UX-specialist should know the basics of programming, should be able to analyze, must constantly grow and learn something new. And I think that it is very inspiring to work! And now I would like to work in a long-term project with interesting new tasks. I hope that my skills can be interesting for your company.`,
+        //aboutText2: `Please, contact me for any information relevant to `,
+        //aboutText3: `my resume`,
+        //aboutText4: `let's move on my works.`,
+        //workTitle: `My works`,
+        //design: `Design`,
+        //prototype: `Prototype`,
+        //coding: `Html coding`,
+        //newsletter: `Email newsletter`,
+        //skillsTitle: `Skills`
     },
 
     link: document.getElementById('interlink'),
 
     changeLang(lang) {
         for (let key in lang) {
-            let selector = `[data-id="${key}"`,
+            let selector = '#'+key,//`[data-id="${key}"`
                 item = document.querySelector(selector);
-
             item.innerText = lang[key]
         }
     },
